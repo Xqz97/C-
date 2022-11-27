413d0fb2a2e5395aa9d7d01695ad87d779242d39
Roman Turusov <turusov97@yandex.ru>
2022-11-27 15:32:10 +0300

    add random

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,3 +1,5 @@
-﻿int number1 = 3;
-int number2 = 65;
+﻿int number1 = new Random().Next(1,10);  // 1 2 3 4 5 ... 9
+int number2 = new Random().Next(1,10);  // 1 2 3 4 5 ... 9
+Console.WriteLine(number1);
+Console.WriteLine(number2);
 Console.WriteLine(number1+number2);
